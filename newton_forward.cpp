669f846188bfa7405971a6d00d03cc583d04607b
MANISH BHARDWAJ <manish0409365@gmail.com>
2017-04-18 20:17:10 +0530

    Create newton_forward.cpp
    THIS PROGRAM IMPLEMENTS THE NEWTON'S FORWARD INTERPOLATION

---
+++ newton_forward.cpp
@@ -0,0 +1,44 @@
+/* THIS PROGRAM IMPLEMENTS THE NEWTON'S FORWARD INTERPOLATION */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+double m[100][100];
+
+int main()
+{
+    cout<<"Enter the number of points\n";
+    int n;
+    cin>>n;
+    cout<<"Enter the value of abscissas and their corresponding functional values\n";
+    double x[n+1],y[n+1],xin,yout=0.0,sum;
+    for(int i=1;i<=n;i++) cin>>x[i]>>y[i];
+    cout<<"Enter the value of x for which y is desired\n";
+    cin>>xin;
+
+    for(int i=1;i<=n;i++) m[i][0]=y[i];
+    for(int j=1;j<n;j++){
+        for(int i=1;i<=n-j;i++){
+            m[i][j]=m[i+1][j-1]-m[i][j-1];
+        }
+    }
+    cout<<"The difference table for given values is\n\n";
+    for(int i=1;i<=n;i++){
+        cout<<x[i]<<"\t";
+        for(int j=0;j<n;j++){
+            cout<<m[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+
+    double x0=x[1],y0=y[1];
+    int k=1;
+    for(int i=2;i<=n;i++)
+        if(x[i]<=xin) x0=x[i],y0=y[i],k=i;
+    double h=x[2]-x[1];
+    double u=(xin-x0)/h;
+    yout=y0;
+    yout += (u*m[k][1]) + ((u*(u-1.0)/2.0)*m[k][2]) + ((u*(u-1.0)*(u-2.0)/6.0)*m[k][3]);;
+    cout<<"\n\nf("<<xin<<") = "<<yout<<endl;
+}
+
